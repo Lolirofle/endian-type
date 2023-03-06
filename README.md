9337653fa4e78eb042a942ea25510a5ee58b2391
Lolirofle <lolipopple@hotmail.com>
2023-03-06 12:12:19 +0100

    Update API (from/to_bytes), add tests.
    - After some thought, the `deref` impls are removed again to keep type safety and possible confusion when writing code.
    - Removed the "parser-implementations" category.
    - `from_bytes`/`to_bytes` is now possible in current Rust.
    - `repr(transparent)` is good to have as an guarantee.
    - Renamed the old byte slice functions and made the old `from_bytes` unsafe, because someone may put an array of incorrect size.
    - Add `const` on some functions. Not possible for everything yet because of feature stabilizations.

--- README.md
+++ README.md
@@ -11,23 +11,31 @@ Type safe wrappers for types with a defined byte order.
 ```rust
 use endian_type::{types, BigEndian, LittleEndian, NetworkOrder};
 
-// the endianness reflects the type-safety of the declaration
+// The endianness reflects the type-safety of the declaration:
 let foo = 0xbeef_u32;
 let foo_be = BigEndian::from(foo);
-assert_eq!(foo_be.as_bytes(), &foo.to_be_bytes());
+assert_eq!(foo_be.to_bytes(), foo.to_be_bytes());
+assert_eq!(foo_be.as_byte_slice(), &foo.to_be_bytes());
 
-// the deref implementations will allow you to operate the wrapped types as if they are regular
-// numbers
+// One can convert back to the native representation using the `From`/`Into` traits:
+assert_eq!(foo, foo_be.into());
+
+// To operate on the wrapped types as if they are regular numbers, one has to
+// be explicit and switch between the byte representations:
+// Note: Internally, these are just [transmutations](https://doc.rust-lang.org/core/mem/fn.transmute.html) and should not affect performance.
 let foo = u128::MAX;
 let foo_le = LittleEndian::from(foo);
-assert_eq!(foo_le.wrapping_add(1), 0);
+assert_eq!(
+    LittleEndian::<u128>::from_bytes(u128::from_ne_bytes(foo_le.to_bytes()).wrapping_add(1).to_ne_bytes()),
+    LittleEndian::<u128>::from(0)
+);
 
-// we also have a couple of aliases to be used as helper
+// We also have a couple of aliases to be used as helper.
 //
-// this will assert our `NetworkOrder` type is in accordance with the IETF RFC1700
+// This will assert our `NetworkOrder` type is in accordance with the IETF RFC1700.
 let foo = -0xdead_i32;
 let foo_no = NetworkOrder::from(foo);
 let foo_be = types::i32_be::from(foo);
-assert_eq!(foo.to_be_bytes(), foo_no.as_bytes());
-assert_eq!(foo.to_be_bytes(), foo_be.as_bytes());
+assert_eq!(foo.to_be_bytes(), foo_no.to_bytes());
+assert_eq!(foo.to_be_bytes(), foo_be.to_bytes());
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #![no_std]
 
-use core::ops::{BitAnd, BitOr, BitXor, Deref, DerefMut};
+use core::ops::{BitAnd, BitOr, BitXor};
 use core::{mem, slice};
 
-///Type with a specified byte order
+///Type with a specified byte order.
 pub trait Endian<T> {}
 
 macro_rules! impl_Endian{
@@ -49,38 +49,32 @@ macro_rules! impl_Endian{
             T: Sized + Copy,
         {
             #[inline]
-            pub fn from_bytes(bytes: &[u8]) -> Self {
+            pub const unsafe fn from_byte_slice(bytes: &[u8]) -> $e<T> {
                 debug_assert!(bytes.len() >= mem::size_of::<T>());
-                $e(unsafe { *(bytes.as_ptr() as *const T) })
+                $e({ *(bytes.as_ptr() as *const T) })
             }
 
             #[inline]
-            pub fn as_bytes(&self) -> &[u8] {
+            pub fn as_byte_slice(&self) -> &[u8] {
                 unsafe { slice::from_raw_parts(&self.0 as *const T as *const u8, mem::size_of::<T>()) }
+            }
+
+            #[inline]
+            pub fn as_byte_slice_mut(&mut self) -> &mut [u8] {
+                unsafe { slice::from_raw_parts_mut(&mut self.0 as *mut T as *mut u8, mem::size_of::<T>()) }
             }
         }
     }
 }
 
-///Big endian byte order
+///Big endian byte order.
 ///
-///Most significant byte first
+///Most significant byte first.
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash, Ord, PartialOrd)]
+#[repr(transparent)]
 pub struct BigEndian<T>(T);
 
 impl<T> Endian<T> for BigEndian<T> {}
-impl<T> Deref for BigEndian<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-impl<T> DerefMut for BigEndian<T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
 
 macro_rules! impl_for_BigEndian {
     ( $t:ident ) => {
@@ -104,6 +98,23 @@ macro_rules! impl_for_BigEndian {
                 BigEndian(data.0.swap_bytes())
             }
         }
+
+        //TODO: Move these to Endian when https://github.com/rust-lang/rust/issues/67792 stabilized in a few years
+        impl BigEndian<$t>{
+            ///Return the memory representation of this type as a byte array in its endian byte order.
+            ///Note: This is just a transmute.
+            #[inline]
+            pub const fn to_bytes(self) -> [u8; mem::size_of::<BigEndian<$t>>()] {
+                unsafe { mem::transmute(self) }
+            }
+
+            ///Construct a value from its memory representation as a byte array.
+            ///Note: This is just a transmute.
+            #[inline]
+            pub const fn from_bytes(bytes: [u8; mem::size_of::<BigEndian<$t>>()]) -> BigEndian<$t> {
+                unsafe { mem::transmute(bytes) }
+            }
+        }
     };
 }
 
@@ -119,25 +130,14 @@ impl_for_BigEndian!(i64);
 impl_for_BigEndian!(i128);
 impl_for_BigEndian!(isize);
 
-///Little endian byte order
+///Little endian byte order.
 ///
-///Least significant byte first
+///Least significant byte first.
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash, Ord, PartialOrd)]
+#[repr(transparent)]
 pub struct LittleEndian<T>(T);
 
 impl<T> Endian<T> for LittleEndian<T> {}
-impl<T> Deref for LittleEndian<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-impl<T> DerefMut for LittleEndian<T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
 
 macro_rules! impl_for_LittleEndian {
     ( $t:ident ) => {
@@ -161,6 +161,22 @@ macro_rules! impl_for_LittleEndian {
                 LittleEndian(data.0.swap_bytes())
             }
         }
+
+        impl LittleEndian<$t>{
+            ///Return the memory representation of this type as a byte array in its endian byte order.
+            ///Note: This is just a transmute.
+            #[inline]
+            pub const fn to_bytes(self) -> [u8; mem::size_of::<LittleEndian<$t>>()] {
+                unsafe { mem::transmute(self) }
+            }
+
+            ///Construct a value from its memory representation as a byte array.
+            ///Note: This is just a transmute.
+            #[inline]
+            pub const fn from_bytes(bytes: [u8; mem::size_of::<LittleEndian<$t>>()]) -> LittleEndian<$t> {
+                unsafe { mem::transmute(bytes) }
+            }
+        }
     };
 }
 
@@ -176,10 +192,10 @@ impl_for_LittleEndian!(i64);
 impl_for_LittleEndian!(i128);
 impl_for_LittleEndian!(isize);
 
-///Network byte order as defined by IETF RFC1700 <http://tools.ietf.org/html/rfc1700>
+///Network byte order as defined by IETF RFC1700 <http://tools.ietf.org/html/rfc1700>.
 pub type NetworkOrder<T> = BigEndian<T>;
 
-///Type aliases for primitive types
+///Type aliases for primitive types.
 pub mod types {
     #![allow(non_camel_case_types)]
 
@@ -219,4 +235,109 @@ pub mod types {
     pub type u64_net = NetworkOrder<u64>;
     pub type u128_net = NetworkOrder<u128>;
     pub type usize_net = NetworkOrder<usize>;
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+    use super::types::*;
+
+    #[test]
+    fn from_to_bytes(){
+        macro_rules! test{
+            ($e:ident for $t:ident in $r:expr) => {
+                for i in $r{
+                    let j = $e::<$t>::from(i);
+                    assert_eq!($e::<$t>::from_bytes(j.to_bytes()) , j);
+                }
+            }
+        }
+
+        test!(BigEndian for i16  in -10000..10000);
+        test!(BigEndian for i32  in -10000..10000);
+        test!(BigEndian for i64  in -10000..10000);
+        test!(BigEndian for i128 in -10000..10000);
+
+        test!(LittleEndian for i16  in -10000..10000);
+        test!(LittleEndian for i32  in -10000..10000);
+        test!(LittleEndian for i64  in -10000..10000);
+        test!(LittleEndian for i128 in -10000..10000);
+
+        test!(BigEndian for u16  in 0..20000);
+        test!(BigEndian for u32  in 0..20000);
+        test!(BigEndian for u64  in 0..20000);
+        test!(BigEndian for u128 in 0..20000);
+
+        test!(LittleEndian for u16  in 0..20000);
+        test!(LittleEndian for u32  in 0..20000);
+        test!(LittleEndian for u64  in 0..20000);
+        test!(LittleEndian for u128 in 0..20000);
+    }
+
+    #[test]
+    fn to_bytes_std(){
+        macro_rules! test{
+            ( for $t:ident in $r:expr) => {
+                for i in $r{
+                    assert_eq!(   BigEndian::<$t>::from(i).to_bytes() , i.to_be_bytes());
+                    assert_eq!(LittleEndian::<$t>::from(i).to_bytes() , i.to_le_bytes());
+                }
+            }
+        }
+
+        test!(for i16  in -10000..10000);
+        test!(for i32  in -10000..10000);
+        test!(for i64  in -10000..10000);
+        test!(for i128 in -10000..10000);
+
+        test!(for i16  in 0..20000);
+        test!(for i32  in 0..20000);
+        test!(for i64  in 0..20000);
+        test!(for i128 in 0..20000);
+    }
+
+    #[test]
+    fn as_to_byte_slice(){
+        macro_rules! test{
+            ($e:ident for $t:ident in $r:expr) => {
+                for i in $r{
+                    let mut j = $e::<$t>::from(i);
+                    assert_eq!(unsafe{$e::<$t>::from_byte_slice(j.as_byte_slice())} , j);
+                    assert_eq!(unsafe{$e::<$t>::from_byte_slice(j.as_byte_slice_mut())} , j);
+                }
+            }
+        }
+
+        test!(BigEndian for i16  in -10000..10000);
+        test!(BigEndian for i32  in -10000..10000);
+        test!(BigEndian for i64  in -10000..10000);
+        test!(BigEndian for i128 in -10000..10000);
+
+        test!(LittleEndian for i16  in -10000..10000);
+        test!(LittleEndian for i32  in -10000..10000);
+        test!(LittleEndian for i64  in -10000..10000);
+        test!(LittleEndian for i128 in -10000..10000);
+
+        test!(BigEndian for u16  in 0..20000);
+        test!(BigEndian for u32  in 0..20000);
+        test!(BigEndian for u64  in 0..20000);
+        test!(BigEndian for u128 in 0..20000);
+
+        test!(LittleEndian for u16  in 0..20000);
+        test!(LittleEndian for u32  in 0..20000);
+        test!(LittleEndian for u64  in 0..20000);
+        test!(LittleEndian for u128 in 0..20000);
+    }
+
+}
+
+#[cfg(doctest)]
+mod test_readme {
+    macro_rules! external_doc_test {
+        ($x:expr) => {
+            #[doc = $x]
+            extern {}
+        };
+    }
+    external_doc_test!(include_str!("../README.md"));
 }
